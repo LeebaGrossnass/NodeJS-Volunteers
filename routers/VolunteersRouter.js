a71dad7f3e9603515d4eb0ddaaf5e2058e4ee3e3
Leeba Grossnass <154058185+LeebaGrossnass@users.noreply.github.com>
2024-10-10 13:04:07 +0300

    Update VolunteersRouter.js

--- routers/VolunteersRouter.js
+++ routers/VolunteersRouter.js
@@ -1,25 +1,9 @@
-//const express = require('express');
 import express from 'express';
 const router = express.Router()
 import controller from '../controllers/VolunteersController.js';
 
-
-// let students = [
-//     { id: 1, name: 'Elisheva', email: 'eli7@gmail.com' },
-//     { id: 2, name: 'Mini', email: 'mini@gmail.com' },
-//     () => { let a = 'b' }
-// ];
-
-//all server definitions will be here
-console.log(3)
 router.get('/', controller.getAll);
-
 router.get('/:id', controller.get);
-
 router.post('/', controller.add);
-
-// router.put('/:id', controller.update);
-
-// router.delete('/:id', controller.delete);
 
 export default router;
