b28844b5125eb7055a954aaed498e6a2ab30b38b
Leeba Grossnass <154058185+LeebaGrossnass@users.noreply.github.com>
2024-10-10 13:02:23 +0300

    Update BaseRepo.js

--- repositories/BaseRepo.js
+++ repositories/BaseRepo.js
@@ -4,7 +4,6 @@ import filter from '../Pipeline/filters.js';
 class BaseRepo {
     constructor(model) {
         this.model = model;
-        //autoBind(this);
         connect();
     }
 
@@ -20,24 +19,12 @@ class BaseRepo {
 
     async get(id) {
         try {
-
-            // let agg = aggregation()
-            // let data2 = await this.model.find({}).exec();
-            // console.log("data2" + data2); 
-            // let data = await this.model.aggregate(agg).exec(); 
-            
              let data = await this.model.findById(id); 
-          
-             console.log(data+"111111111111111111111111");
              return data;  
         }
-
         catch (errors) {
             throw errors;
         }
     }
-
 }
-
-//module.exports = BaseRepo;
 export default BaseRepo;
